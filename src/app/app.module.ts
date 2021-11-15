d434de0e68c9aec8d73b9021dceea141502adbc8
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-15 23:15:28 +0400

    Icon assets & SVG sprites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 
 @NgModule({
   declarations: [AppComponent, SidebarComponent, ContentLayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, IconSpriteModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './page/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { InvoiceItemComponent } from './components/invoice-item/invoice-item.component';
 import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
-
 import { HomeRoutingModule } from './home-routing.module';
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,6 @@ import { HomeRoutingModule } from './home-routing.module';
     InvoiceItemComponent,
     InvoiceFormComponent,
   ],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, IconSpriteModule],
 })
 export class HomeModule {}
