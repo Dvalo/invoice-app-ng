fda3cf6f10eca3a4b6d83400c6412d76e042ae25
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-18 01:08:07 +0400

    Fix data display on inner page header

--- src/app/modules/invoice/components/header/header.component.ts
+++ src/app/modules/invoice/components/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Invoice } from 'src/app/data/schema/invoice';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() invoice!: Invoice;
+  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
