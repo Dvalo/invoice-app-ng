8dacd61b0feeb9921a0b8d55071274e101682df4
Dvalo <guram.dvalishvili99@gmail.com>
2021-12-14 14:07:49 +0400

    Update invoice count on homepage header

--- src/app/modules/home/components/header/header.component.ts
+++ src/app/modules/home/components/header/header.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { DrawerService } from 'src/app/shared/services/drawer.service';
+import { InvoiceService } from 'src/app/data/service/invoice.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  invoiceCount!: number;
 
-  constructor(private drawerSerivce: DrawerService) { }
+  constructor(
+    private invoiceService: InvoiceService,
+    private drawerSerivce: DrawerService
+  ) {}
 
   ngOnInit(): void {
+    this.invoiceService.getInvoices().subscribe((data) => {
+      this.invoiceCount = data.length;
+    });
   }
 
   openDrawer() {
     this.drawerSerivce.openDrawer();
   }
-
 }
